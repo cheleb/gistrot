99607d531f07e563e054c46d474c058de073a835
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-04-15 08:38:21 +0200

    lihaoyi lib 0.5 native

--- app/src/main/scala/Main.scala
+++ app/src/main/scala/Main.scala
@@ -1,5 +1,5 @@
-// import fansi.*
-// import fansi.Color.*
+import fansi.*
+import fansi.Color.*
 import libgit.functions.*
 import libgit.types.*
 
@@ -16,29 +16,23 @@ import java.nio.charset.StandardCharsets
 // ++ Black(Back.Green("\uE0C0 "))
 //
 @main def gistrotPrompt(spaces: Int, fill: String) =
+  val project =
+    if os.exists(os.pwd / "build.sbt") then LightRed(" \ue737")
+    else Str("")
 
-  println("cococococo")
-  val project = "zozo"
-  // if os.exists(os.pwd / "build.sbt") then LightRed(" \ue737")
-  // else Str("")
-  // if os.exists(os.pwd / "build.sbt") then " \ue737"
-  // else ""
+  val pwd =
+    if (os.pwd.startsWith(os.home)) then
+      Blue(
+        "\ue0c6 /" + os.pwd.relativeTo(os.home).toString
+      )
+    else LightGray("\ueb06 ") ++ Blue(os.pwd.toString)
 
-  // val pwd =
-  //   if (os.pwd.startsWith(os.home)) then
-  //     Blue(
-  //       "\ue0c6 /" + os.pwd.relativeTo(os.home).toString
-  //     )
-  //   else LightGray("\ueb06 ") ++ Blue(os.pwd.toString)
-  val pwd = ""
-  // if (os.pwd.startsWith(os.home)) then
+  if (os.pwd.startsWith(os.home)) then
+    "\ue0c6 /" + os.pwd.relativeTo(os.home).toString
+  else "\ueb06 " ++ os.pwd.toString
 
-  //   "\ue0c6 /" + os.pwd.relativeTo(os.home).toString
-  // else "\ueb06 " ++ os.pwd.toString
-
-  Zone { implicit z: Zone =>
+  Zone {
     git_libgit2_init()
-    println("cococococo")
 
     LibGit2.repo(c".").map { repo =>
 
@@ -74,8 +68,7 @@ import java.nio.charset.StandardCharsets
       val right = s" $diffs"
       val path = pwd ++
         project ++
-        // Green(" " + branch)
-        " " + branch
+        Green(" " + branch)
 
       s"$path${fill * (spaces - path.length - right.length)}$right"
 
